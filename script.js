6b157dbc8eac622ff0ace306eec9277cf58937cf
Trayana Hansen <92487819+trayana-hansen@users.noreply.github.com>
2022-09-05 12:49:41 +0200

    half way commit

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-let btnOne = document.getElementById(ex1);
-let h1 = document.getElementsByTagName("h1");
-btnOne.addEventListener("click", )
+document.getElementById('ex1').addEventListener("click", function () {
+	document.getElementsByTagName('h1')[0].innerHTML = "Hello World"
+});
+document.getElementById("ex2").addEventListener("click", function () {
+	let paragraphs = document.querySelectorAll("p").forEach(function (element) {
+		element.classList.toggle("red");
+	})
+})
+document.getElementById('ex3').addEventListener('click', function () {
+	document.querySelectorAll("li").forEach(function (element) {
+		element.classList.toggle("rotate");
+	});
+})
+
+document.getElementById('ex4').addEventListener('click', function () {
+	document.querySelectorAll("h1, h2, h3, h4, h5, h6").forEach(function (element) {
+		element.classList.toggle("hide");
+	});
+})
+document.getElementById('ex5').addEventListener('click', function () {
+	document.querySelectorAll("a").forEach(function (element) {
+		element.classList.toggle("font");
+	});
+})
